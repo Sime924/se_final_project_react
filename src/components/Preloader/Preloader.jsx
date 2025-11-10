20a19fb96c987296443ae6cc585758456e4f8521
Simon Gebord <sgebord39@gmail.com>
2025-11-10 14:14:58 -0500

    Preloader and fonts inststyles are imported.

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,10 @@
+import "./Preloader.css";
+
+function Preloader() {
+  return (
+    <div className="preloader">
+      <div className="circle-preloader"></div>
+      <p className="Preloader-text">Searching for news...</p>
+    </div>
+  );
+}
