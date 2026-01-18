6166b8943a03d55bf0b5c7ec59ec70222e353281
Simon Gebord <sgebord39@gmail.com>
2026-01-18 17:35:42 -0500

    footer main page complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import "./Footer.css";
+import githubLogo from "../../assets/github-logo.svg";
+import linkedinLogo from "../../assets/linkedin-logo.svg";
 
 function Footer() {
   return (
@@ -11,12 +13,32 @@ function Footer() {
         <div className="footer__links">
           <button className="footer__btn">Home</button>
           <a
-            className="footer__triple-ten"
+            className="footer__btn"
             href="https://tripleten.com"
             target="_blank"
             rel="noopener noreferrer"
           >
             TripleTen
+          </a>
+        </div>
+        <div className="footer__icons">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/sime924"
+          >
+            <img className="footer__icon" src={githubLogo} alt="github__logo" />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener no referrer"
+            href="https://linkedin.com"
+          >
+            <img
+              className="footer__icon"
+              src={linkedinLogo}
+              alt="linkedin logo"
+            />
           </a>
         </div>
       </div>
